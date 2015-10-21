15530ac4eeff9d272e100b02c62ce5bb1f55d0ed
Tim Hacker <tim.hacker@trueclarity.co.uk>
2015-10-21 14:02:27 +0100

    Gulpfile working and dist folder removed and ignored

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ gulp.task('connect', function() {
 
 gulp.task('open', ['connect'], function() {
   gulp.src('dist/index.html')
-    .pipe(open({ url: config.developmentBaseUrl + ':' + config.port + '/'}));
+    .pipe(open({ uri: config.developmentBaseUrl + ':' + config.port + '/'}));
 });
 
 
