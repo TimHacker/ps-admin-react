f45e6ea2ff249db03e6f494fde3d09a8ae5a1725
Tim Hacker <tim.hacker@trueclarity.co.uk>
2015-10-21 14:15:14 +0100

    Browserify added and test Javascript file added

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,18 @@
 var gulp = require('gulp');
 var gulpConnect = require('gulp-connect'); //Runs a local dev server
 var open = require('gulp-open'); //Open a URL in a web browser
+var browserify = require('browserify'); // Bundles JS
+var reactify = require('reactify'); // Transforms React JSX to JS
+var source = require('vinyl-source-stream'); // Use conventional text streams with Gulp
 
 var config = {
   port: 9005,
   developmentBaseUrl: 'http://localhost',
   paths: {
     html: './src/*.html',
-    dist: './dist'
+    js: './src/**/*.js',
+    dist: './dist',
+    mainJs: './src/main.js'
   }
 }
 
@@ -35,8 +40,19 @@ gulp.task('html', function() {
     .pipe(gulpConnect.reload());
 });
 
-gulp.task('watch', function() {
-  gulp.watch(config.paths.html, ['html']);
+gulp.task('js', function() {
+  browserify(config.paths.mainJs)
+    .transform(reactify)
+    .bundle()
+    .on('error', console.error.bind(console))
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(config.paths.dist + '/scripts'))
+    .pipe(gulpConnect.reload());
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('watch', function() {
+  gulp.watch(config.paths.html, ['html']);
+  gulp.watch(config.paths.js, ['js']);
+});
+
+gulp.task('default', ['html', 'js', 'open', 'watch']);
