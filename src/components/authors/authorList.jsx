1a740192519da80382b650d851952925f2ef8d52
Tim Hacker <timothyhacker@gmail.com>
2015-10-27 12:35:35 +0000

    Added PropTypes validation to AuthorList
    This validation means that the authors property must be an array and it
    must exist

--- src/components/authors/authorList.jsx
+++ src/components/authors/authorList.jsx
@@ -4,6 +4,10 @@ var React = require('react');
 var Link = require('react-router').Link;
 
 var AuthorList = React.createClass({
+  propTypes: {
+    authors: React.PropTypes.array.isRequired
+  },
+
   render: function() {
     var createAuthorRow = function(author) {
       return (
