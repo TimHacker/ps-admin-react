4aa02f962afe646f4b3138a216af233eae213601
Tim Hacker <timothyhacker@gmail.com>
2015-10-29 15:10:45 +0000

    Created basic Flux store implementation
    Initial boilerplate version that uses EventEmitter and uses the s6
    ponyfill Object
    Assign to merge them together with our AuthorStore.
    This is using the Node events implementation:
    https://nodejs.org/api/events.html

---
+++ src/stores/authorStore.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var Dispatcher = require('../dispatcher/appDispatcher');
+var ActionTypes = require('../constants/actionTypes');
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
+var CHANGE_EVENT = 'change';
+
+var AuthorStore = assign({}, EventEmitter.prototype, {
+  addChangeListener: function(callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
+
+  removeChangeListener: function(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  },
+
+  emitChange: function() {
+    this.emit(CHANGE_EVENT);
+  }
+});
+
+Dispatcher.register(function(action) {
+  switch (action.actionType) {
+    case ActionTypes.CREATE_AUTHOR:
+
+      break;
+    default:
+
+  }
+});
+
+module.exports = AuthorStore;
